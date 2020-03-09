1df01f983ecae71dc838c67f0db87d1cffba82c7
weru <onewesh@gmail.com>
2020-03-09 21:50:10 +0300

    try scrollview

--- moonshot/ContentView.swift
+++ moonshot/ContentView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            GeometryReader { geo in
-            Image("roosa")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: geo.size.width)
+        ScrollView(.vertical) {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    Text("Item \($0)")
+                        .font(.title)
+                }
             }
+            .frame(maxWidth: .infinity)
         }
     }
 }
