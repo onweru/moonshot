5f2f60148c523d26118c21dc204f0fbd8e6e2c40
weru <onewesh@gmail.com>
2020-03-09 22:15:53 +0300

    save work

---
+++ Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  moonshot
+//
+//  Created by dan on 3/9/20.
+//  Copyright © 2020 dan. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
